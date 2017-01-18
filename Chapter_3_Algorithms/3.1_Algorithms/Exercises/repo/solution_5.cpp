77990769fe8478e098204ad7a978459412919250
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-01-19 03:11:38 +1100

    C++ Algo solution for problem 5

--- Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/solution_5.cpp
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/solution_5.cpp
@@ -6,8 +6,55 @@
   Date: Jan 19th, 2017
  */
 
+#include "merge_sort.h"
+#include "common.h"
+#include <set>
+#include <cstdio>
+
+std::vector<int> multiple_occurences_normal(const std::vector<int>& list) {
+    std::vector<int> duplicates;
+    std::size_t i = 0;
+    while (i < list.size()) {
+        if (list[i] == list[i + 1])
+            duplicates.push_back(list[i]);
+
+        int j = i;
+        while (list[j] == list[i]) ++j;
+        i = j;
+    }
+    
+    return duplicates;
+}
+
+std::set<int> multiple_occurences_set(const std::vector<int>& list) {
+    std::set<int> duplicates;
+    for (std::size_t i = 0; i < list.size() - 1; ++i)
+        if (list[i] == list[i + 1])
+            duplicates.insert(list[i]);
+    return duplicates;
+}
+
+void test_multiple_occurences() {
+    discrete_mathematics::chapter_3::MergeSort<int> sort;
+    discrete_mathematics::chapter_3::Common<int> common;
+
+    for (int i = 0; i < 10; ++i) {
+        printf("------------------------------------------------------------------\n");
+        std::vector<int> list = common.build_vector(50, 0, 100);
+        sort.sort(list);
+        common.print_vector(list);
+        printf("Elements in the list more than once are\n");
+        common.print_vector(multiple_occurences_normal(list));
+        const std::set<int> duplicates = multiple_occurences_set(list);
+        for (std::set<int>::const_iterator it = duplicates.begin();
+             it != duplicates.end(); ++it)
+            printf("%d ", *it);
+        printf("\n------------------------------------------------------------------\n");
+    }
+}
+
 int main() {
-
-
+    test_multiple_occurences();
+    
     return 0;
 }
