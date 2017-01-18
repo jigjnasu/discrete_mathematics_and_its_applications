7e8eaeefae509c114a3ec1cb2493430c94219136
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-01-19 01:51:31 +1100

    Adding common class

---
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/common.h
@@ -0,0 +1,62 @@
+/*
+  This class contains some of the common functionality used in all the exercises here.
+  Author: Rakesh Kumar (cpp.rakesh(at)gmail.com)
+  Date: 19th, Jan 2017
+ */
+
+#ifndef DISCRETE_MATHEMATICS_AND_ITS_APPLICATIONS_CHAPTER_3_3_1_EXERCISES_REPO_COMMON_H_
+#define DISCRETE_MATHEMATICS_AND_ITS_APPLICATIONS_CHAPTER_3_3_1_EXERCISES_REPO_COMMON_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+namespace discrete_mathematics {
+    namespace chapter_3 {
+        template <typename T>
+        class Common {
+        public:
+            Common();
+            ~Common();
+
+            // return a random number between min and max, both included
+            int random(int min, int max) const;
+
+            // print the vector
+            void print_vector(const std::vector<T>& V) const;
+
+            // Build a random vector of n elements between min and max elements
+            std::vector<T> build_vector(int n, int min, int max) const;
+        };
+    };
+};
+
+namespace dc = discrete_mathematics::chapter_3;
+
+template <typename T>
+dc::Common<T>::Common() {}
+
+template <typename T>
+dc::Common<T>::~Common() {}
+
+template <typename T>
+int dc::Common<T>::random(int min, int max) const {
+    return min + rand() % (max - min + 1);
+}
+
+template <typename T>
+void dc::Common<T>::print_vector(const std::vector<T>& V) const {
+    for (std::size_t i = 0; i < V.size(); ++i)
+        std::cout << V[i] << " ";
+    std::cout << std::endl;
+}
+
+template <typename T>
+std::vector<T> dc::Common<T>::build_vector(int n, int min, int max) const {
+    std::vector<T> list;
+    for (int i = 0; i < n; ++i)
+        list.push_back(random(min, max));
+    return list;
+}
+
+#endif // DISCRETE_MATHEMATICS_AND_ITS_APPLICATIONS_CHAPTER_3_3_1_EXERCISES_REPO_COMMON_H_
--- Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/solution_3.cpp
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/solution_3.cpp
@@ -6,28 +6,12 @@
   Date: Jan 19th, 2017
  */
 
+#include "common.h"
 #include <cstdio>
 #include <vector>
 #include <cstdlib>
 
-int random(int min, int max) {
-    return min + rand() % (max - min + 1);
-}
-
-void print(const std::vector<int>& list) {
-    printf("[List]: ");
-    for (std::size_t i = 0; i < list.size(); ++i)
-        printf("%d ", list[i]);
-    printf("\n");
-}
-
-std::vector<int> build_list() {
-    std::vector<int> list;
-    for (int i = 0; i < 50; ++i)
-        list.push_back(random(10, 200));
-    return list;
-}
-
+// Sum of all elements of set S
 int sum(const std::vector<int>& list) {
     int sum = 0;
     int i = 0;
@@ -44,10 +28,12 @@ int sum(const std::vector<int>& list) {
     return sum;
 }
 
+// Test cases
 void test_sum() {
+    discrete_mathematics::chapter_3::Common<int> common;
     for (int i = 0; i < 10; ++i) {
-        const std::vector<int> list = build_list();
-        print(list);
+        const std::vector<int> list = common.build_vector(50, 10, 200);
+        common.print_vector(list);
         printf("Sum of all the elements of the list == [%d]\n\n", sum(list));
     }
 }
