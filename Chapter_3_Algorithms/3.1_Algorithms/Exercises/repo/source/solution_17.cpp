8954243eed99e12f587336779015bfd586e8719a
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-01-20 01:36:55 +1100

    Comment modifciation

--- Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/source/solution_17.cpp
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/source/solution_17.cpp
@@ -1,7 +1,7 @@
 /*
   Discrete Mathematics and Its Applications by (Kenneth H. Rosen)
   Chapter 3.1
-  Solution for problem 16
+  Solution for problem 17
   Author: Rakesh Kumar (cpp.rakesh(at)gmail.com)
   Date: Jan 19th, 2017
  */
---
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/source/solution_18.cpp
@@ -0,0 +1,10 @@
+/*
+  Discrete Mathematics and Its Applications by (Kenneth H. Rosen)
+  Chapter 3.1
+  Solution for problem 17
+  Author: Rakesh Kumar (cpp.rakesh(at)gmail.com)
+  Date: Jan 19th, 2017
+ */
+
+#include "common.h"
+#include <cstdio>
