8db57111029b8cdc3b1839c20ec7c3f262442316
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-01-22 22:51:47 +1100

    Solution for 33

---
+++ Chapter_3_Algorithms/3.1_Algorithms/Exercises/repo/source/solution_33.cpp
@@ -0,0 +1,52 @@
+/*
+  Discrete Mathematics and Its Applications by (Kenneth H. Rosen)
+  Chapter 3.1
+  Solution for problem 33
+  Author: Rakesh Kumar (cpp.rakesh(at)gmail.com)
+  Date: Jan 22nd, 2017
+ */
+
+#include "common.h"
+#include <cstdio>
+#include <map>
+#include <vector>
+
+std::map<int, int> find_first_least_number(const std::vector<int>& list) {
+    std::map<int, int> result;
+    for (std::size_t i = 1; i < list.size(); ++i) {
+        if (list[i] < list[i - 1]) {
+            result.insert(std::pair<int, int>(list[i], i + 1));
+            break;
+        }
+    }
+
+    return result;
+}
+
+void test_first_first_least_number() {
+    int min = 1;
+    int max = 100;
+    int limit = 100;
+    discrete_mathematics::chapter_3::Common<int> common;
+
+    std::vector<int> list;
+    for (int i = 0; i < limit; ++i)
+        list.push_back(common.random(min, max));
+
+    const std::map<int, int> result = find_first_least_number(list);
+
+    common.print(list);
+    printf("------------------------------------------------------------------\n");
+    printf("First term which is less than the previous term\n");
+    for (std::map<int, int>::const_iterator it = result.begin();
+         it != result.end(); ++it)
+        printf("[%d] at [%d]\n", it->first, it->second);
+    printf("------------------------------------------------------------------\n");    
+}
+
+
+int main() {
+    test_first_first_least_number();
+
+    return 0;
+}
