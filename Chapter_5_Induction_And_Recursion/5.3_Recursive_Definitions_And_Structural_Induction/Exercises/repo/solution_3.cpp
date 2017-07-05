4b9625a179b65f6bf23d52b9890bda48adc55749
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-06 03:12:50 +1000

    adding solution 3

--- Chapter_5_Induction_And_Recursion/5.3_Recursive_Definitions_And_Structural_Induction/Exercises/repo/solution_3.cpp
+++ Chapter_5_Induction_And_Recursion/5.3_Recursive_Definitions_And_Structural_Induction/Exercises/repo/solution_3.cpp
@@ -23,63 +23,76 @@ int pow(int x, int y) {
     }
 }
 
-// Basic     Step f(0)     = 1
-// Recursive Step f(n + 1) = f(n) + 2
+// Basic     Step f(0)     = -1
+// Basic     Step f(1)     = 2
+// Recursive Step f(n + 1) = f(n) + 3f(n - 1)
 int a(int n) {
     if (n == 0)
-        return 1;
+        return -1;
+    else if (n == 1)
+        return 2;
     else
-        return a(n - 1) + 2;
+        return a(n - 1) + 3 * a(n - 2);
 }
 
-// Basic     Step f(0)     = 1
-// Recursive Step f(n + 1) = 3f(n)
+
+// Basic     Step f(0)     = -1
+// Basic     Step f(1)     = 2
+// Recursive Step f(n + 1) = f(n) ^ 2 f(n - 1)
 int b(int n) {
     if (n == 0)
-        return 1;
+        return -1;
+    else if (n == 1)
+        return 2;
     else
-        return 3 * b(n - 1);
+        return b(n - 1) * b (n - 1) * b (n - 2);
 }
 
-// Basic     Step f(0)     = 1
-// Recursive Step f(n + 1) = 2 ^ f(n)
-int c(int n) {
+// Basic     Step f(0)     = -1
+// Basic     Step f(1)     = 2
+// Recursive Step f(n + 1) = 3f(n) ^ 2 - 4f(n - 1) ^ 2
+long long int c(int n) {
     if (n == 0)
-        return 1;
+        return -1;
+    else if (n == 1)
+        return 2;
     else
-        return pow(2, c(n - 1));
+        return (3 * c(n - 1) * c(n - 1)) - (4 * c(n - 2) * c (n - 2));
 }
 
-// Basic     Step f(0)     = 1
-// Recursive Step f(n + 1) = f(n) ^ 2 + f(n) + 1
-int d(int n) {
+// Basic     Step f(0)     = -1
+// Basic     Step f(1)     = 2
+// Recursive Step f(n + 1) = f(n - 1) / f(n)
+float d(int n) {
     if (n == 0)
-        return 1;
+        return -1;
+    else if (n == 1)
+        return 2;
     else
-        return d(n - 1) * d(n - 1) + d(n - 1) + 1;
+        return d(n - 2) / d(n - 1);
 }
 
 void test() {
-    const int max = 5;
-    printf("----- f(n + 1) = f(n) + 2 -------\n");
+    const int max = 6;
+    printf("----- f(n + 1) = f(n) + 3f(n - 1) -- \n");
     for (int n = 0; n < max; ++n)
         printf("a(%2d) == [%8d]\n", n, a(n));
     printf("---------------------------------\n");
 
-    printf("----- f(n + 1) = 3 * f(n) -------\n");
+    printf("----- f(n + 1) = f(n) ^ 2 f(n - 1) -\n");
     for (int n = 0; n < max; ++n)
         printf("b(%2d) == [%8d]\n", n, b(n));
     printf("---------------------------------\n");
 
-    printf("----- f(n + 1) = 2 ^ f(n) -------\n");
+    printf("----- f(n + 1) = 3f(n)^2 -4f(n - 1)^2\n");
     for (int n = 0; n < max; ++n)
-        printf("c(%2d) == [%8d]\n", n, c(n));
+        printf("c(%2d) == [%8lld]\n", n, c(n));
     printf("---------------------------------\n");
 
-    printf("-- f(n + 1) = f(n) ^ 2 + f(n) + 1\n");
+    printf("-- f(n + 1) = f(n - 1) / f(n)\n");
     for (int n = 0; n < max; ++n)
-        printf("d(%2d) == [%8d]\n", n, d(n));
-    printf("---------------------------------\n");     
+        printf("d(%2d) == [%4.4f]\n", n, d(n));
+    printf("---------------------------------\n");
 }
 
 int main() {
