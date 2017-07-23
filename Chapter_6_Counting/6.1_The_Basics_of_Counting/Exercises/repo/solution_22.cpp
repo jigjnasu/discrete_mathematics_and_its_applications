5b5b178a8768a3a3169c6a6a150c951a116d857a
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-24 02:03:38 +1000

    soltution 22

--- Chapter_6_Counting/6.1_The_Basics_of_Counting/Exercises/repo/solution_22.cpp
+++ Chapter_6_Counting/6.1_The_Basics_of_Counting/Exercises/repo/solution_22.cpp
@@ -9,6 +9,7 @@
 
 #include <cstdio>
 #include <vector>
+#include <string>
 
 inline void print(const std::vector<int>& numbers) {
     printf("-----------------------------------------------------\n");
@@ -48,6 +49,50 @@ std::vector<int> solution_d(int s, int e, int a, int b) {
     return n;
 }
 
+std::vector<int> solution_e(int s, int e, int a, int b) {
+    std::vector<int> n;
+    for (int i = s; i < e; ++i)
+        if ((i % a == 0 || i % b == 0) && (i % (a * b)))
+            n.push_back(i);
+    
+    return n;
+}
+
+std::vector<int> solution_f(int s, int e, int a, int b) {
+    std::vector<int> n;
+    for (int i = s; i < e; ++i)
+        if (!(i % a == 0 || i % b == 0))
+            n.push_back(i);
+    
+    return n;
+}
+
+bool is_distinct(int n) {
+    if (n < 10)
+        return true;
+    else if (n >= 10 && n <= 99)
+        return n % 10 != n / 10;
+    else
+        return (((n / 100) != ((n / 10) % 10)) && ((n / 100) != (n % 10)) && (((n / 10) % 10) != (n % 10)));
+}
+
+std::vector<int> solution_g(int s, int e) {
+    std::vector<int> n;
+    for (int i = s; i < e; ++i)
+        if (is_distinct(i))
+            n.push_back(i);
+    return n;
+}
+
+std::vector<int> solution_h(int s, int e) {
+    const std::vector<int> n = solution_g(s, e);
+    std::vector<int> r;
+    for (std::size_t i = 0; i < n.size(); ++i)
+        if (n[i] % 2 == 0)
+            r.push_back(n[i]);
+    return r;
+}
+
 int gcd(int a, int b) {
     if (a % b == 0)
         return b;
@@ -67,6 +112,7 @@ void solution_22() {
     int e = 1000;
     int g = 7;
     std::vector<int> a = range(s, e, g);
+    printf("------------------ Solution a) --------------------\n");
     printf("[%lu] elements between [%d] - [%d] divisible by [%d]\n", a.size(), s, e, g);
     print(a);
 
@@ -75,6 +121,7 @@ void solution_22() {
     e = 1000;
     g = 7;
     std::vector<int> b = solution_b(a);
+    printf("------------------ Solution b) --------------------\n");
     printf("[%lu] elements between [%d] - [%d] divisible by [%d] not by [11]\n", b.size(), s, e, g);
     print(b);
 
@@ -83,6 +130,7 @@ void solution_22() {
     e = 1000;
     g = lcm(11, 7);
     std::vector<int> c = range(s, e, g);
+    printf("------------------ Solution c) --------------------\n");
     printf("[%lu] elements between [%d] - [%d] divisible by [%d]\n", c.size(), s, e, g);
     print(c);
 
@@ -91,8 +139,39 @@ void solution_22() {
     s = 1;
     e = 1000;
     std::vector<int> d = solution_d(s, e, 7, 11);
+    printf("------------------ Solution d) --------------------\n");
     printf("[%lu] elements between [%d] - [%d] divisible by [7 or 11]\n", d.size(), s, e);
     print(d);
+
+    // Solution e)
+    std::vector<int> result = solution_e(1, 1000, 7, 11);
+    printf("------------------ Solution e) --------------------\n");
+    printf("[%lu] elements between [1] - [1000] divisible by [7 or 11 not by 77]\n", result.size());
+    print(result);
+
+    // Solution f)
+    s = 1;
+    e = 1000;
+    std::vector<int> f = solution_f(s, e, 7, 11);
+    printf("------------------ Solution f) --------------------\n");
+    printf("[%lu] elements between [%d] - [%d] NOT divisible by [7 or 11]\n", f.size(), s, e);
+    print(f);
+
+    // Solution g)
+    s = 1;
+    e = 1000;
+    std::vector<int> r = solution_g(s, e);
+    printf("------------------ Solution g) --------------------\n");
+    printf("Districnt elements [%lu] between [%d] - [%d]\n", r.size(), s, e);
+    print(r);
+    
+    // Solution h)
+    s = 1;
+    e = 1000;
+    std::vector<int> h = solution_h(s, e);
+    printf("------------------ Solution h) --------------------\n");
+    printf("Districnt elements [%lu] between [%d] - [%d]\n", h.size(), s, e);
+    print(h);    
 }
 
 int main() {
