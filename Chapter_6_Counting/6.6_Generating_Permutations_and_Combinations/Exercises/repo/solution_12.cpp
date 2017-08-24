4d763e2f1e3965fc40d05455c1d4e971fa87851a
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-08-25 01:29:56 +1000

    solution 12

---
+++ Chapter_6_Counting/6.6_Generating_Permutations_and_Combinations/Exercises/repo/solution_12.cpp
@@ -0,0 +1,68 @@
+/*
+  Discrete Mathematics and Its Applications
+  by Dr. Kenneth H. Rosen
+  Chapter 6.6 Generating Permutations and Combinations
+  Solution 12
+  Rakesh Kumar, cpp.rakesh(at)gmail.com
+  25/08/2017
+ */
+
+#include <cstdio>
+#include <string>
+
+inline void swap(char& a, char& b) {
+    a ^= b;
+    b ^= a;
+    a ^= b;
+}
+
+// Let's permute the set s = {};
+void permute(std::string& s) {
+    printf("---------------------------------\n");
+    while (1) {
+        printf("%s\n", s.c_str());
+
+        int i = s.size() - 2;
+        while (i >= 0 && s[i] > s[i + 1])
+            --i;
+        if (i < 0) break;
+
+        int j = s.size() - 1;
+        while (s[i] > s[j])
+            --j;
+
+        swap(s[i], s[j]);
+        
+        int f = i + 1;
+        int l = s.size() - 1;
+
+        while (f < l) {
+            if (s[f] > s[l])
+                swap(s[f], s[l]);
+            ++f;
+            --l;
+        }
+    }
+    printf("---------------------------------\n");
+}
+
+void r_permute(std::string& s, int r) {
+    for (std::size_t i = 0; i <= s.size() - r; ++i) {
+        for (std::size_t j = i + 1; j <= s.size() - r + 1; ++j) {
+            std::string p;
+            p.push_back(s[i]);
+            p.push_back(s[j]);            
+            for (std::size_t k = 0; k < r - 2; ++k) {
+                p.push_back(s[k + j + 1]);
+            }
+
+            permute(p);
+        }
+    }
+}
+
+int main() {
+    std::string s = "123456789";
+    r_permute(s, 5);
+    return 0;
+}
