88ac008bc95823709bd4c50d77477d36797a6710
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-06-09 23:01:49 +1000

    solution 4

---
+++ Chapter_8_Advanced_Counting_Techniques/8.3_Divide_and_Conquer_Algorithms_and_Recurrence_Relations/Exercises/repo/solution_4.cpp
@@ -0,0 +1,27 @@
+/*
+  Discrete Mathematics and Its Applications
+  by Dr. Kenneth H. Rosen
+  Chapter 8.3 Divide and Conquer Algorithms and Recurrence Relations
+  Solution 4
+  Rakesh Kumar, cpp.rakesh@gmail.com
+  09/06/2018
+ */
+
+#include <bits/stdc++.h>
+
+int size(int n) {
+    int s = 0;
+    while (n)
+        n >>= 1;
+
+    return s;
+}
+
+int fast_multiply(int a, int b) {
+    printf("[%d] || [%d] && [%d] == [%d]\n", )
+}
+
+int main() {
+
+    return 0;
+}
