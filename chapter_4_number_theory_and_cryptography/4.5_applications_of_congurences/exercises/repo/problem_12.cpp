03fbcebd54b0614b70b75222a76d37b58e30d7b9
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-07-11 02:06:33 +1000

    resturctuing of code

--- chapter_4_number_theory_and_cryptography/4.5_applications_of_congurences/exercises/repo/problem_12.cpp
+++ chapter_4_number_theory_and_cryptography/4.5_applications_of_congurences/exercises/repo/problem_12.cpp
@@ -1,4 +1,4 @@
-#include "PowerRandom.h"
+#include "power_random.h"
 #include <cstdio>
 
 int main() {
@@ -7,11 +7,11 @@ int main() {
     int x = 3;
     discrete_mathematics::chapter_4::PowerRandom random;
     const std::vector<int> numbers = random.generate(p, d, x);
-    
+
     printf("----------------------------------------------\n");
     for (std::size_t i = 0; i < numbers.size(); ++i)
         printf("%d ", numbers[i]);
-    printf("\n----------------------------------------------\n");    
-    
+    printf("\n----------------------------------------------\n");
+
     return 0;
 }
