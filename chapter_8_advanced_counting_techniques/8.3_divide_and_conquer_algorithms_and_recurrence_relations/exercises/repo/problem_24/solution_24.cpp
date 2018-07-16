bf4d88743f232162d39a85671e1d861bd1739c39
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-07-17 03:41:10 +1000

    solution 24 iterative

---
+++ chapter_8_advanced_counting_techniques/8.3_divide_and_conquer_algorithms_and_recurrence_relations/exercises/repo/problem_24/solution_24.cpp
@@ -0,0 +1,10 @@
+/*
+  Discrete Mathematics and Its Applications
+  by Dr. Kenneth H. Rosen
+  Chapter 8.3 Divide and Conquer Algorithms and Recurrence Relations
+  Solution 24
+  Rakesh Kumar, cpp.rakesh@gmail.com
+  17/07/2018
+ */
+
+#include <bits/stdc++.h>
