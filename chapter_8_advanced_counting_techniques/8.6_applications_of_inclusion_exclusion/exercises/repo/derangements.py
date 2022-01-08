94ff5990b0d005b5d64d78cd6507fa30839209ac
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-01-09 10:15:11 +1100

    derangements

---
+++ chapter_8_advanced_counting_techniques/8.6_applications_of_inclusion_exclusion/exercises/repo/derangements.py
@@ -0,0 +1,29 @@
+'''
+    Discrete Mathematics and its applications
+    Chapter 8.6
+    Implementation of derangements
+'''
+def fact(n):
+    r = 1
+    for i in range(1, n + 1):
+        r = r * i
+    return r
+
+def derangements(n):
+    nf = fact(n)
+    r = 0
+    print('{} - {} '.format(nf, nf), end='')
+    for i in range(2, n + 1):
+        t = fact(i)
+        t = nf // t
+        if i & 1 == 0:
+            print('+ {} '.format(t), end='')
+            r = r + t
+        else:
+            print('- {} '.format(t), end='')    
+            r = r - t
+    print()
+    return r
+
+if __name__ == '__main__':
+    print(derangements(int(input())))
