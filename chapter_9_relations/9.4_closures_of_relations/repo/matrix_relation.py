770861ed26a90d3fa129163a2863903a9ff18208
Rakesh Kumar <cpp.rakesh@gmail.com>
2023-03-21 05:55:31 +1100

    matrix relation code O(n^3)

--- chapter_9_relations/9.4_closures_of_relations/repo/matrix_relation.py
+++ chapter_9_relations/9.4_closures_of_relations/repo/matrix_relation.py
@@ -4,7 +4,7 @@
 
 class MR(object):
     def __init__(self, no_of_elements_in_set, ordered_pairs):
-        self._M = [ [0]*no_of_elements_in_set for i in range(no_of_elements_in_set)]
+        self._M = [[0]*no_of_elements_in_set for i in range(no_of_elements_in_set)]
         for e in ordered_pairs:
             r = e[0] - 1
             c = e[1] - 1
@@ -13,10 +13,12 @@ class MR(object):
     def R(self, n):
         T = self._M
         for i in range(1, n):
+            M = self._empty_matrix()
             for r in range(len(self._M)):
                 for c in range(len(self._M)):
                     if self._bit_set(r, c, T):
-                        T[r][c] = 1
+                        M[r][c] = 1
+            T = M
         return T
     
     def _bit_set(self, r, c, T):
@@ -24,6 +26,9 @@ class MR(object):
             if self._M[r][i] == 1 and T[i][c] == 1:
                 return True
         return False
+    
+    def _empty_matrix(self):
+        return [[0]*len(self._M) for i in range(len(self._M))]
     
 def print_matrix(M):
     print('-------------------------------------')
@@ -33,14 +38,12 @@ def print_matrix(M):
         print('')
     print('-------------------------------------')
 
-def test():
+def solution_19():
     op = [(1, 3), (2, 4), (3, 1), (3, 5), (4, 3), (5, 1),(5, 2), (5, 4)]
     mr = MR(5, op)
-    R = mr.R(1)
-    print_matrix(R)
-    
-    T = mr.R(2)
-    print_matrix(R)
+    for i in range(1, 7):
+        R = mr.R(i)
+        print_matrix(R)
 
 if __name__ == '__main__':
-    test()
+    solution_19()
